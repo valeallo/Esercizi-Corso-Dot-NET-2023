ee2a09117d86d241a2fb92f3fba014035df579ac
valeallo <valentina.allodi@gmail.com>
2023-11-07 14:51:53 +0100

    made one random change to try commit inside vs git tools

--- Rdc-calculator/Program.cs
+++ Rdc-calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Rdc_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!!!!!!");
         }
     }
 }
