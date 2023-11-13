80cfd34a34845bbffcb20ad8b706aa9a254911c4
valeallo <valentina.allodi@gmail.com>
2023-11-13 10:00:49 +0100

    added state and EuropeanState Classes

--- Rdc-Calculator/Eurozone-Calculator/Program.cs
+++ Rdc-Calculator/Eurozone-Calculator/Program.cs
@@ -7,6 +7,14 @@ namespace Eurozone_Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            EuropeanState germany = new EuropeanState("Germany", true);
+            germany.DisplayInformation();
+
+            EuropeanState norway = new EuropeanState("Norway", false);
+            norway.DisplayInformation();
         }
+        
     }
+
 }
---
+++ Rdc-Calculator/Eurozone-Calculator/StateClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace Eurozone_Calculator
+{
+	 abstract class State
+	{
+        string _name;
+
+
+        protected State(string Name)
+        {
+            Name = _name;
+        }
+
+        public string Name { get { return _name; } }
+
+        public abstract void DisplayInformation();
+    }
+
+    class EuropeanState : State
+    {
+
+        bool _isEurozone;
+ 
+        public EuropeanState(string name, bool isEurozone) : base(name)
+        {
+            _isEurozone = isEurozone;
+        }
+
+        public bool IsEurozone { get { return _isEurozone; } }
+
+        public override void DisplayInformation()
+        {
+            Console.WriteLine($"State: {Name}, Eurozone Member: {IsEurozone}");
+        }
+    }
+}
