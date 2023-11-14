9884ab35e8d1d416467de81628a9d83ecb5b4278
valeallo <valentina.allodi@gmail.com>
2023-11-14 12:45:17 +0100

    eurozoneState extends IBCE

--- Rdc-Calculator/Associations/EurozoneState.cs
+++ Rdc-Calculator/Associations/EurozoneState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Associations.interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Associations
 {
-    internal class EurozoneState : EUState
+    internal class EurozoneState : EUState, IBCE
     {
         public EurozoneState(string Name, EuropeanUnion europeanUnion) : base(Name, europeanUnion)
         {
