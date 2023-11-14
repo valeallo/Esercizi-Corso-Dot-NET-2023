d8c056f13bcdbfc3ed60f59c986acedde6a2a669
valeallo <valentina.allodi@gmail.com>
2023-11-14 14:07:27 +0100

    onu and Eu inherit from political organization

--- Rdc-Calculator/Associations/interfaces/IEuropeanUnion.cs
+++ Rdc-Calculator/Associations/interfaces/IEuropeanUnion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Associations.interfaces
 {
-    internal interface IEuropeanUnion
+    internal interface IEuropeanUnion : IPoliticalOrganization
     {
     }
 }
--- Rdc-Calculator/Associations/interfaces/IONU.cs
+++ Rdc-Calculator/Associations/interfaces/IONU.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Associations.interfaces
 {
-    interface IONU
+    interface IONU : IPoliticalOrganization
     {
     }
 }
