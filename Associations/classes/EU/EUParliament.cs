07d7af312bc53a4cfacb879e4e22306aee065b4b
valeallo <valentina.allodi@gmail.com>
2023-11-16 12:36:43 +0100

    added eu parliament approval method

--- Associations/classes/EU/EUParliament.cs
+++ Associations/classes/EU/EUParliament.cs
@@ -1,4 +1,5 @@
-﻿using Associations.interfaces.UE;
+﻿using Associations.classes.Default;
+using Associations.interfaces.UE;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace Associations.classes.UE
 {
-    internal class EUParliament : IEUAdministrativeEntity
+    internal class EUParliament 
     {
+
+        public EUParliament() { }
+
+
+        public bool ApproveChanges()
+        {
+            Random random = new Random();
+            return random.Next(2) == 1;
+        }
+
     }
 }
