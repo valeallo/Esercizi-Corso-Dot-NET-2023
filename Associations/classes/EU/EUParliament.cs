b178a7dfa45ff4fcc702b1638e19cb212bbcfb11
valeallo <valentina.allodi@gmail.com>
2023-11-16 11:22:45 +0100

    added euparliament class

---
+++ Associations/classes/EU/EUParliament.cs
@@ -0,0 +1,13 @@
+﻿using Associations.interfaces.UE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associations.classes.UE
+{
+    internal class EUParliament : IEUAdministrativeEntity
+    {
+    }
+}
