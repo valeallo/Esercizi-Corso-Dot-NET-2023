35f2e1547d4671cdb3a50c048159b3662fd98fb5
valeallo <valentina.allodi@gmail.com>
2023-11-21 11:56:59 +0100

    add new album, get all albums

---
+++ SpotifyClone/Artist.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyClone
+{
+    internal class Artist : User
+    {
+        string _alias;
+        Album[] _albums;
+        private int _albumCount;
+
+        public Artist (string name, string Alias) : base (name) 
+        { 
+            _alias = Alias;
+            _albumCount = 0;
+            _albums = new Album[0];
+        }
+
+        public string Alias { get { return _alias; } }
+
+        public void AddAlbum(Album album)
+        {
+            if (_albumCount == _albums.Length)
+            {
+                int newSize = _albums.Length + 1;
+                Array.Resize(ref _albums, newSize);
+            }
+
+            _albums[_albumCount++] = album;
+        }
+
+
+        public Album[] GetAllAlbums()
+        {
+            Album[] currentAlbums = new Album[_albumCount];
+            Array.Copy(_albums, currentAlbums, _albumCount);
+            return currentAlbums;
+        }
+
+    }
+}
