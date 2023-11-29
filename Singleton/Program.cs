629d53f75d00e135a4964285d4ea79cb149df647
valeallo <valentina.allodi@gmail.com>
2023-11-29 10:09:58 +0100

    completed singleton exercise, singleton contains 4 ip address, check in 2 instances if the ip addresses are the same

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Singleton
 {
@@ -6,7 +7,51 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N = 3;
+            ProxyServer proxy1 = ProxyServer.GetInstance();
+            ProxyServer proxy2 = ProxyServer.GetInstance();
+
+            List<string> ipAddressesFromProxy1 = new List<string>();
+            List<string> ipAddressesFromProxy2 = new List<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                ipAddressesFromProxy1.Add(proxy1.GetIPAddress(i));
+                ipAddressesFromProxy2.Add(proxy2.GetIPAddress(i));
+            }
+
+            bool listsAreEqual = true;
+
+            if (ipAddressesFromProxy1.Count == ipAddressesFromProxy2.Count)
+            { 
+                for (int i = 0; i < ipAddressesFromProxy1.Count; i++)
+                {
+                    if (ipAddressesFromProxy1[i] != ipAddressesFromProxy2[i])
+                    {
+                        listsAreEqual = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+           
+                listsAreEqual = false;
+            }
+
+
+            if (listsAreEqual)
+            {
+                Console.WriteLine("Le liste sono uguali.");
+            }
+            else
+            {
+                Console.WriteLine("Le liste sono diverse.");
+            }
+
+
+
+
         }
     }
 }
