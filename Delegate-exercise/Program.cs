6da154224f572440c743b3123a1e0267d5302a24
valeallo <valentina.allodi@gmail.com>
2023-12-18 12:09:56 +0100

    primo esercizio fatto

--- Delegate-exercise/Program.cs
+++ Delegate-exercise/Program.cs
@@ -6,33 +6,36 @@ namespace Delegate_exercise
    internal class Program
         {
 
+    
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            //primo esercizio : 
+            SumDelegate sumDel =  Sum;
+            PrintSumDelegate execSumDel = new PrintSumDelegate(StringifyResult);
+            string result = execSumDel(sumDel);
+            PrintResultInConsole(result);
+ 
+        }
+
+        //primo esercizio : 
         public delegate int SumDelegate(int a, int b);
         public delegate string PrintSumDelegate(SumDelegate sumFunction);
-
-       
         public static int Sum(int a, int b)
         {
             return a + b;
         }
-
         public static string StringifyResult(SumDelegate sumFunction)
         {
             return sumFunction(5, 10).ToString();
         }
-
-        static void Main(string[] args)
+        public static void PrintResultInConsole(string str)
         {
-            SumDelegate sumDel = new SumDelegate(Sum);
-
-            PrintSumDelegate execSumDel = new PrintSumDelegate(StringifyResult);
-
-      
-            string result = execSumDel(sumDel);
-
-       
-            Console.WriteLine("The result is: " + result);
-
- 
+            Console.WriteLine("The result is: " + str);
         }
     }
 
