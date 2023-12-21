ed160db8cf0a72706b5de05d8d1a6c797c51496e
valeallo <valentina.allodi@gmail.com>
2023-12-21 16:08:16 +0100

    added serilogger but have to finish configuration and install pachkages

---
+++ Spotify.API/Spotify.API/Repository/AlbumRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Spotify.API.Models;
+
+namespace Spotify.API.Repository
+{
+    public class AlbumRepository
+    {
+
+            private readonly ILogger<UserRepository> _logger;
+            private readonly SpotifyContext _context;
+
+            public AlbumRepository(ILogger<UserRepository> logger, SpotifyContext context)
+            {
+                _logger = logger;
+                _context = context;
+            }
+        }
+    }
+}
